e58dfe942583be50f04886bf4855caa786149fce
Ron <ron@libp45p-22306dy.mis.lmig.com>
2017-03-15 19:50:40 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+// Display values of any two cards to console
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);

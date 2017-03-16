81a624aa60c377d289b7370a5db1fb98440827b9
Ron Larson <larson5.ron@gmail.com>
2017-03-16 17:37:29 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* UNIT 7 
 console.log("Up and running!");
 
 var cardOne = "queen";
@@ -8,3 +9,34 @@ var cardFour = "king";
 // Display values of any two cards to console
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+*/
+
+// UNIT 8
+var cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+
+// First flipped card
+cardOne = cards[0];
+
+// Add cardOne to cardsInPlay array
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+// Second flipped card
+cardTwo = cards[2];
+
+// Add cardTwo to cardsInPlay array
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+// Check if 2 cards have been played
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+
+

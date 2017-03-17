b310ae6fa1ae1de8df29d62e7df2608ee37ab44d
Ron Larson <larson5.ron@gmail.com>
2017-03-16 21:26:35 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -11,26 +11,15 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 */
 
-// UNIT 8
+// UNIT 8 and 9
+
+// Global Variables
 var cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
 
-// First flipped card
-cardOne = cards[0];
-
-// Add cardOne to cardsInPlay array
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-// Second flipped card
-cardTwo = cards[2];
-
-// Add cardTwo to cardsInPlay array
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-// Check if 2 cards have been played
-if (cardsInPlay.length === 2) {
+// Function: checkForMatch
+// Purpose: Checks for a match of 2 cards
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
@@ -38,5 +27,46 @@ if (cardsInPlay.length === 2) {
 	}
 }
 
+// Function: flipCard
+// Purpose: User flips a card
+var flipCard = function (cardId) {
 
+	// UNIT 8 - Commented out
+	/*
+	// First flipped card
+	var cardOne = cards[0];
 
+	// Add cardOne to cardsInPlay array
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	// Second flipped card
+	cardTwo = cards[2];
+
+	// Add cardTwo to cardsInPlay array
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+	*/
+
+    // Display card that was flipped over
+    console.log("User flipped " + cards[cardId]);
+
+    // Add flipped card to cardsInPlay array
+    cardsInPlay.push(cards[cardId]);
+
+	// Check if 2 cards have been played
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		/* UNIT 8 - Moved to checkForMatch function in UNIT 9
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.")
+		}
+		*/
+	}
+}
+
+// Simulate user flipping a card
+flipCard(0);  // Flip first card in card array
+flipCard(2);  // Flip third card in card array
